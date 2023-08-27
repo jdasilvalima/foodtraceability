e7af21a688f2a1032305f997a7f1876f285ec8c2
jade <jade.dasilva.lima.jds@gmail.com>
2023-08-27 16:32:20 -0400

    added pagination to product table

--- web3/components/Navbar.tsx
+++ web3/components/Navbar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar: React.FC = () => {
+
+  const pathname = usePathname();
+
   return (
     <nav className="bg-lime-200 p-4 flex justify-between items-center">
       <div className="text-2xl font-bold">
@@ -9,9 +15,9 @@ const Navbar: React.FC = () => {
         </Link>
       </div>
       <div className="space-x-4">
-        <Link href="/supplychain">SupplyChain</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/users">Users</Link>
+        <Link className={pathname === '/supplychain' ? 'active' : ''} href="/supplychain">SupplyChain</Link>
+        <Link className={pathname === '/products' ? 'active' : ''} href="/products">Products</Link>
+        <Link className={pathname === '/users' ? 'active' : ''} href="/users">Users</Link>
       </div>
     </nav>
   );
---
+++ web3/components/ProductsTable.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react'
+import productList from '@/data/products.json'
+
+const ProductsTable: React.FC = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 5;
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = productList.slice(indexOfFirstProduct, indexOfLastProduct);
+
+   const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <div className="max-h-[28rem] overflow-x-auto">
+      <table>
+        <thead>
+          <tr>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Product ID
+            </th>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Name
+            </th>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Origin
+            </th>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Quantity
+            </th>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Unit
+            </th>
+            <th className="px-6 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 font-medium text-gray-500 uppercase tracking-wider">
+              Timestamp
+            </th>
+          </tr>
+        </thead>
+        <tbody className="overflow-y-auto">
+          {currentProducts.map((product) => (
+            <tr key={product.productId}>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.productId}</td>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.name}</td>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.origin}</td>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.quantity}</td>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.unit}</td>
+              <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">{product.timestamp}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="mt-4">
+        <ul className="pagination flex justify-end">
+          {Array.from({ length: Math.ceil(productList.length / productsPerPage) }, (_, index) => (
+            <li
+              key={index}
+              className={`px-3 py-1 cursor-pointer ${
+                index + 1 === currentPage ? 'bg-lime-400 text-white' : 'hover:bg-lime-200'
+              }`}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsTable;
