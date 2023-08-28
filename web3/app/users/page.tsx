f04a092af50a4c0bfebb237b08314fbf4905fd25
jade <jade.dasilva.lima.jds@gmail.com>
2023-08-28 02:52:40 -0400

    added users list

--- web3/app/users/page.tsx
+++ web3/app/users/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import UserCard from '@/components/UserCard'
+import userList from '@/data/users.json'
 
 export default function Users() {
   return (
-    <div>Users page</div>
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Users List</h1>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {userList.map((user, index) => (
+          <UserCard key={index} user={user} />
+        ))}
+      </div>
+    </div>
   )
 }
---
+++ web3/components/UserCard.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+interface User {
+  name: string;
+  location: string;
+  role: number;
+}
+
+const UserCard: React.FC<{ user: User }> = ({ user }) => {
+
+  const roleMapping = {
+    0: 'None',
+    1: 'Farmer',
+    2: 'Distributor',
+    3: 'Retailer',
+    4: 'Consumer',
+    5: 'Laboratory',
+    6: 'Auditor',
+  };
+
+  const userRole = roleMapping[user.role];
+
+  return (
+    <div className="max-w-sm border-2 border-slate-400 rounded-xl overflow-hidden">
+      <div className="md:flex">
+        <div className="flex items-center">
+          <img className="max-h-28 w-full object-cover" src="/sheaf-of-rice-emoji.png" alt={`${user.name}'s profile`} />
+        </div>
+        <div className="p-8">
+          <h2 className="mt-2 text-xl font-semibold">{user.name}</h2>
+          <div className="uppercase tracking-wide text-sm">{userRole}</div>
+          <p className="mt-2 text-gray-500">{user.location}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
