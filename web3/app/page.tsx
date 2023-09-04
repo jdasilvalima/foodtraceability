99019b91c2b815451ff66de5a7ea1cc84cedea83
jade <jade.dasilva.lima.jds@gmail.com>
2023-09-04 16:17:58 -0400

    connexion with Metamask

--- web3/app/page.tsx
+++ web3/app/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from 'next/image'
-import Link from 'next/link';
+import Web3 from 'web3';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const loadWeb3 = async () => {
+    // checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.ethereum !== "undefined") {
+      // use MetaMask's provider
+      const web3 = new Web3(window.ethereum);
+      try {
+        // request the user to connect their MetaMask account to the web application
+        await window.ethereum.enable();
+        const accounts = await web3.eth.getAccounts();
+        const userAddress = accounts[0];
+        // TODO save user
+        router.push("/products");
+      } 
+      catch (error) {
+        console.error("User denied account access:", error);
+      }
+    } else {
+      console.error("MetaMask is not installed.");
+    }
+  };
+
   return (
     <div className="flex p-2">
       <div className="flex-1 self-center mr-10">
@@ -9,11 +37,12 @@ export default function Home() {
           Food traceability from <br />
           farmer to customer
         </div>
-        <Link href="/products">
-          <button className="bg-lime-200 hover:bg-lime-300 font-bold rounded-lg p-3 mt-5">
-            Connect to Metamask
-          </button>
-        </Link>
+        <button 
+          className="bg-lime-200 hover:bg-lime-300 font-bold rounded-lg p-3 mt-5"
+          onClick={loadWeb3}
+        >
+          Connect to Metamask
+        </button>
       </div>
       <div className="flex-1">
         <Image
